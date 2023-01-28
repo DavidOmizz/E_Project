b4cb4490f4bdc51897439f430ddc567310c6631e
David Omisakin <davidomisakin4good@gmail.com>
2023-01-29 00:30:56 +0100

    done

--- my_app/forms.py
+++ my_app/forms.py
@@ -2,12 +2,12 @@ from django import forms
 from .models import Order
 
 class OrderForm(forms.ModelForm):
-    # customer_name = forms.CharField(label = 'name', widget= forms.TextInput(attrs={'placeholder':'Enter full name', 'class': 'form-control', 'required':True}))
-    # customer_number = forms.CharField(label = 'number', widget= forms.NumberInput(attrs={'placeholder':'Enter your number', 'class': 'form-control', 'minlength': 11, 'type': 'number', 'required':True}))
-    # customer_email = forms.EmailField(label = 'email', widget= forms.TextInput(attrs={'placeholder':'Enter your mail', 'class': 'form-control', 'type': 'email'}))
-    # message = forms.CharField(label = 'content', widget= forms.Textarea(attrs={'placeholder':'Message', 'class': 'form-control','required':True}))
-    # shipping_address = forms.CharField(label = 'shipping_address', widget = forms.TextInput(attrs={'placeholder':'Shipping Address', 'class': 'form-control','minlength':10, 'required':True}))
-    # quantity = forms.CharField(label = 'quantity', widget= forms.NumberInput(attrs={'placeholder':'Enter the quantity', 'class': 'form-control','required':True}))
+    customer_name = forms.CharField(label = 'Full Name', widget= forms.TextInput(attrs={'placeholder':'Enter full name', 'class': 'form-control', 'required':True}))
+    customer_number = forms.CharField(label = 'Mobile no.', widget= forms.NumberInput(attrs={'placeholder':'Enter your mobile number', 'class': 'form-control', 'minlength': 11, 'type': 'number', 'required':True}))
+    customer_email = forms.EmailField(label = 'Email', widget= forms.TextInput(attrs={'placeholder':'Enter your mail', 'class': 'form-control', 'type': 'email'}))
+    message = forms.CharField(label = 'Message', widget= forms.Textarea(attrs={'placeholder':'Send us messages about your order', 'class': 'form-control','required':True}))
+    shipping_address = forms.CharField(label = 'Shipping Address', widget = forms.TextInput(attrs={'placeholder':'Enter your shipping address', 'class': 'form-control','minlength':10, 'required':True}))
+    quantity = forms.CharField(label = 'Quantity', widget= forms.NumberInput(attrs={'placeholder':'Enter the quantity', 'class': 'form-control','required':True}))
     class Meta:
         model = Order
         fields = ('customer_name', 'customer_number','customer_email','quantity','message','shipping_address')
